a929f9e7d9013859454239f1c73d5bca8b8a928a
Prashanthshe <41041692+Prashanthshe@users.noreply.github.com>
2018-10-09 22:02:18 +0530

    Add files via upload

---
+++ Assignment 12.1.R
@@ -0,0 +1,16 @@
+write.csv(yeast,"C:/Users/prash/Desktop/yeast.csv")
+
+#Ques.1.a. Perform ANOVA test on the discriminant analysis scores of nuclear localization signals of both nuclear
+#and non-nuclear proteins by class variables (Target).
+
+a<-aov(yeast$mit~yeast$nuc)
+summary(a)
+
+#Ques.1.b.Which class is significantly different from others?
+
+plot(yeast$mcg~yeast$class)
+aa<-aov(yeast$gvh~yeast$class)
+summary(aa)
+TukeyHSD(aa)
+
+#the class is significantly different which p>0.05
